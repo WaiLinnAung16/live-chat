4db9038a86c4cd3ea47b81ecd9934a8d0f662f71
WaiLinnAung18 <waia00198@gmail.com>
2022-12-03 10:49:28 +0630

    setup and connect firebase

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFkMSSqurG_tWBopVFZtrhtEdUjpSnfzE",
+  authDomain: "vue-blog-test-4ea9e.firebaseapp.com",
+  projectId: "vue-blog-test-4ea9e",
+  storageBucket: "vue-blog-test-4ea9e.appspot.com",
+  messagingSenderId: "802067532817",
+  appId: "1:802067532817:web:32f5fcd5ebb634eb8775b0",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { db, timestamp };
